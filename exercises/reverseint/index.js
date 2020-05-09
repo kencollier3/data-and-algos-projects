abf50f3495940e1869a93d5d261c88ba180343ff
kencollier3 <mckennoncollier@gmail.com>
2020-05-09 17:56:51 -0600

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reverseNum = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+    return parseInt(reverseNum) * Math.sign(n);
+}
 
 module.exports = reverseInt;
