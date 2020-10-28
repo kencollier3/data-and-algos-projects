00edcf27b84b7167ab32c4c6586db793c01c5eb9
kencollier3 <mckennoncollier@gmail.com>
2020-10-28 10:11:42 -0600

    vowels

--- completed_exercises/vowels/index.js
+++ completed_exercises/vowels/index.js
@@ -13,16 +13,3 @@ function vowels(str) {
 }
 
 module.exports = vowels;
-
-// function vowels(str) {
-//   let count = 0;
-//   const checker = ['a', 'e', 'i', 'o', 'u'];
-//
-//   for (let char of str.toLowerCase()) {
-//     if (checker.includes(char)) {
-//       count++;
-//     }
-//   }
-//
-//   return count;
-// }
