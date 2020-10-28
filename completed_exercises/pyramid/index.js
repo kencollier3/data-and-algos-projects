51eb690ff2a9c6d619baacb20f5eaa733a828742
kencollier3 <mckennoncollier@gmail.com>
2020-10-28 10:10:21 -0600

    pyramids

--- completed_exercises/pyramid/index.js
+++ completed_exercises/pyramid/index.js
@@ -14,42 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row = 0, level = '') {
-  if (row === n) {
-    return;
-  }
+function pyramid(n) {
+  const LEVEL_CALC = 2 * n - 1;
+  const midpoint = Math.floor(LEVEL_CALC / 2);
 
-  if (level.length === 2 * n - 1) {
-    console.log(level);
-    return pyramid(n, row + 1);
-  }
+  for (let row = 0; row < n; row++) {
+    let level = '';
 
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  let add;
-  if (midpoint - row <= level.length && midpoint + row >= level.length) {
-    add = '#';
-  } else {
-    add = ' ';
+    for (let column = 0; column < LEVEL_CALC; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
   }
-  pyramid(n, row, level + add);
 }
-
 module.exports = pyramid;
-//
-// function pyramid(n) {
-//   const midpoint = Math.floor((2 * n - 1) / 2);
-//
-//   for (let row = 0; row < n; row++) {
-//     let level = '';
-//
-//     for (let column = 0; column < 2 * n - 1; column++) {
-//       if (midpoint - row <= column && midpoint + row >= column) {
-//         level += '#';
-//       } else {
-//         level += ' ';
-//       }
-//     }
-//
-//     console.log(level);
-//   }
-// }
